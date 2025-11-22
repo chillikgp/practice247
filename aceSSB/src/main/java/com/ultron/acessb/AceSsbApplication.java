6e2538038520496fced87b52df36797c8f99a7f9
pankajThakur1874 <pankajthakur1874@gmail.com>
2025-11-22 12:39:51 +0530

    initial commit

---
+++ aceSSB/src/main/java/com/ultron/acessb/AceSsbApplication.java
@@ -0,0 +1,13 @@
+package com.ultron.acessb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceSsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceSsbApplication.class, args);
+    }
+
+}
---
+++ aceSSB/src/test/java/com/ultron/acessb/AceSsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultron.acessb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AceSsbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
